85665a29bcd712aab465ddb280baf250647764a9
dvassil <dvassil@users.noreply.github.com>
2022-05-19 17:23:25 +0300

    require could/should be out of loop

--- require.php
+++ require.php
@@ -1,11 +1,11 @@
 <?php
 $start = microtime();
 
-for ($x = 0; $x < 100000; $x++) {
-	require_once 'class1.php';
-	require_once 'class2.php';
-	require_once 'class3.php';
+require_once 'class1.php';
+require_once 'class2.php';
+require_once 'class3.php';
 
+for ($x = 0; $x < 100000; $x++) {
 	$class1 = new Class1();
 	$class2 = new Class2();
 	$class3 = new Class3();
